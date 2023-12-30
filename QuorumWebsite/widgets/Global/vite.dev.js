40df9b4b263d57a258a0d4f539f5454328a3145b
Brandon Fidler <brandonfidler57@gmail.com>
2023-12-30 12:46:17 -0800

    removes unneeded config file

--- QuorumWebsite/widgets/Global/vite.dev.js
+++
@@ -1,9 +0,0 @@
-// vite.dev.js
-import configs from './config/generateConfigs.js';
-
-// Pick one of the configs based on an environment variable or some other condition
-const selectedConfigIndex = process.env.VITE_WIDGET_INDEX || 0;
-const selectedConfig = configs[selectedConfigIndex];
-
-export default selectedConfig;
-
